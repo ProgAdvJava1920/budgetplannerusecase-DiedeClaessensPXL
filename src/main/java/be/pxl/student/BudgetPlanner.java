305e161acd8391f0ae5b3ab0b34cd963cd2b5902
DiedeClaessensPXL <43785242+DiedeClaessensPXL@users.noreply.github.com>
2020-03-25 14:07:26 +0100

    Update BudgetPlanner.java

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -2,7 +2,46 @@ package be.pxl.student;
 
 public class BudgetPlanner {
     public static void main(String[] args) {
+         private static void getPayments(String name){
+                EntityManagerFactory entityManagerFactory = null;
+                EntityManager entityManager = null;
+                try {
+                    entityManagerFactory = Persistence.createEntityManagerFactory("paymentsdb_pu");
+                    entityManager = entityManagerFactory.createEntityManager();
 
+                    TypedQuery<Payment> query = entityManager.createQuery("SELECT payment from Payment payment", Payment.class);
+                }
+                finally {
+                    if (entityManager != null) {
+                        entityManager.close();
+                    }
+                    if (entityManagerFactory != null) {
+                        entityManagerFactory.close();
+                    }
+                }
+            }
+         private static void importCsv(){
+        LOGGER.debug("Started reading file");
+        BudgetPlannerImporter importer = new BudgetPlannerImporter();
+        LOGGER.debug("Stopped reading file");
+
+        EntityManagerFactory entityManagerFactory = null;
+        EntityManager entityManager = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("paymentsdb_pu");
+            entityManager = entityManagerFactory.createEntityManager();
+
+            importer.importCvs(Paths.get("./src/main/resources/account_payments_v2.csv"), entityManager);
+        }
+        finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+            if (entityManagerFactory != null) {
+                entityManagerFactory.close();
+            }
+        }
+    }
     }
 
 
